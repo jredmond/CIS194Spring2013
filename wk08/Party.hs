f5b2705aa0dd2775c5f62b8d98f6d6100a4b821c
John Redmond <redmondjohnf@gmail.com>
2015-06-02 11:40:53 +0930

    wk08 q4-5

--- wk08/Party.hs
+++ wk08/Party.hs
@@ -24,13 +24,51 @@ moreFun :: GuestList -> GuestList -> GuestList
 moreFun gl1 gl2 = if gl1 > gl2 then gl1 else gl2
 
 -- Exercise 2
-treeFold :: (a -> b -> b) -> b -> Tree a -> b
-treeFold f z (Node a ts) = foldl (treeFold f) (f a z) ts
+-- treeFold' :: Tree Employee -> (GuestList, GuestList)
+-- treeFold' (Node e []) = nextLevel e [(mempty,mempty)]
+-- treeFold' (Node e ts) = nextLevel e (map treeFold' ts)
+
+treeFold :: (a -> [b] -> b) -> b -> Tree a -> b
+treeFold f z (Node x []) = f x [z]
+treeFold f z (Node x ts) = f x (map (treeFold f z) ts)
 
 -- Exercise 3
 -- Takes boss and list of results for subtree's under boss
 -- Returns pair of GuestLists: (Best with boss, Best without boss)
 -- (Including boss gives all subtree bosses 0 fun)
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel boss gls = 
-  (glCons boss (foldr ((<>) . snd) mempty gls), foldr ((<>) . fst) mempty gls)
+nextLevel boss gls = (bestWithBoss, bestWithoutBoss)
+  where
+    bestWithBoss    = glCons boss (foldr ((<>) . snd) mempty gls)
+    bestWithoutBoss = foldr ((<>) . fst) mempty gls
+
+-- Exercise 4
+maxFun :: Tree Employee -> GuestList
+maxFun = bestFromPair . rootBestGLs
+  where
+    rootBestGLs = treeFold nextLevel (mempty,mempty)
+    bestFromPair (gl1,gl2) = moreFun gl1 gl2
+
+-- Exercise 5
+main :: IO ()
+main = do
+    inpStr <- readFile "company.txt"
+    let guestList = maxFun . buildTree $ inpStr
+    putStrLn ("Total fun: " ++ show (getFun guestList))
+    mapM_ putStrLn $ getNames guestList
+
+buildTree :: String -> Tree Employee
+buildTree = read
+
+getFun :: GuestList -> Fun
+getFun (GL _ fun) = fun
+
+getNames :: GuestList -> [Name]
+getNames (GL es _) = map empName es
+
+
+
+
+
+
+
